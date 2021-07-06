47ab1ff69c271c4622a495a26be53e850ee99325
Ágata Velasco Penze <avpenze@gmail.com>
2021-07-06 10:21:39 -0400

    Finalizando pagina Sign In

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Input, Button } from '@chakra-ui/react'
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -18,10 +18,45 @@ export default function Home() {
         borderRadius={8}
         flexDir="column"
       >
-        <Input name="email" type="email" />
-        <Input name="password" type="password" />
+      
 
-        <Button type="submit" mt="6">Entrar</Button>
+      <Stack spacing="4">
+
+        <FormControl>
+          <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email" 
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+                size="lg"
+            />
+
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="password">Senha</FormLabel>
+          <Input
+          id="password"
+          name="password"
+          type="password"
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: "gray.900"
+          }}
+          size="lg"
+          />
+        </FormControl>
+        
+      </Stack>
+        <Button type="submit" colorScheme="pink" mt="6" size="lg">Entrar</Button>
       </Flex>
     </Flex>
   )
