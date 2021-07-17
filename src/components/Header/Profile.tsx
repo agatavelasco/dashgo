a2e148331acdfc95b40997cb65b7fc618651721a
Ágata Velasco Penze <avpenze@gmail.com>
2021-07-16 23:47:33 -0400

    Aplicando responsividade no Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile () {
+interface ProfileProps {
+  showProfileData?: boolean; 
+}
+
+export function Profile ({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-    <Box mr="4" textAlign="right">
-      <Text>Agata Velasco Penze</Text>
-      <Text color="gray.300" fontSize="small">
-        avpenze@gmail.com
-      </Text>
-    </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+        <Text>Agata Velasco Penze</Text>
+          <Text color="gray.300" fontSize="small">
+            avpenze@gmail.com
+          </Text>
+        </Box>
+      ) }
     <Avatar size="md" name="Agata Velasco" src="https://github.com/agatavelasco.png" />
   </Flex>
   );
