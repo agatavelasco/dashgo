9b9f46854dacf982f08ccd7084f6412be9ebd3e1
Ágata Velasco Penze <avpenze@gmail.com>
2021-07-28 20:18:12 -0400

    Logica de paginacao

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import faker from 'faker'
 
 type User = { //como se User fosse a tabela e as colunas: name, email, created_at
@@ -28,7 +28,7 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 10) // criar 10 usuarios
+      server.createList('user', 200) // criar 10 usuarios
     },
 
     routes() {
@@ -36,7 +36,23 @@ export function makeServer() {
       this.namespace = 'api'; // a rota para acessar será /api/users
       this.timing = 750; // toda requisição feita ao mirage irá demorar 750 milisegundos
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) -1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user'))
+          .users.slice(pageStart, pageEnd)
+
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        )
+      });
       this.post('/users');
 
       this.namespace = ''; // reseta o namespace para nao prejudicar caso haja pasta api no next
